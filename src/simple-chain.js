0ff22f147d1ad17cc671f80b57b19cfca571f955
FullCircleRoid <andrewthechf@gmail.com>
2020-10-05 02:15:26 +0300

    feat: add chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+class ChainMaker {
+    constructor() {
+        this.arr = []
+        this.resultArr = []
+    }
+
+    getLength() {
+        return this
+    }
+
+    addLink(value) {
+      if (value === undefined) {
+        this.arr.push(' ')
+        return this
+      }
+        this.arr.push(value)
+        return this
+    }
+
+    removeLink(position) {
+      if (this.IsInteger(position) && this.arr[position] !== undefined && position > 0) {
+        if (position === 0) {
+          this.arr.splice(position, 1)
+          return this
+        } else {
+          this.arr.splice(position - 1, 1)
+          return this
+        }
+      }
+      this.arr = []
+      this.resultArr = []
+      throw new Error('THROWN')
+    }
+
+    reverseChain() {
+      this.arr.reverse()
+      return this
+    }
+
+    finishChain() {
+        this.clue()
+        let result = this.resultArr.join('')
+        this.arr = []
+        this.resultArr = []
+        return result
+    }
+
+    clue() {
+      if (this.arr.length === 1) {
+        this.resultArr.push(`( ${this.arr[0]} )`)
+      } else {
+
+        for (let i = 0; i < this.arr.length; i++) {
+          if (i === 0) {
+            this.resultArr.push(`( ${this.arr[i]} )~`)
+          } else if (i === this.arr.length - 1) {
+            this.resultArr.push(`~( ${this.arr[i]} )`)
+          } else {
+            this.resultArr.push(`~( ${this.arr[i]} )~`)
+          }
+        }
+      }
+    }
+
+    IsInteger(value) {
+        return (value ^ 0 ) === value;
+    }
+}
+
+let chainMaker = new ChainMaker()
 
 module.exports = chainMaker;
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  if (date === undefined ) {
+    console.log(date)
+    return 'Unable to determine the time of year!'
+  }
+  if (!(date instanceof Date)) {
+    new Error('THROWN')
+  }
+
+  let month = date.getMonth() + 1
+  console.log('date', date)
+  console.log('month', month)
+  console.log('==========================')
+  if (month <= 2) {
+    return 'winter'
+  } else if  (month > 2 && month <= 5){
+    return 'spring'
+  } else if  (month > 5 && month <= 9){
+    return 'summer'
+  } else {
+    return 'autumn'
+  }
+
 };
