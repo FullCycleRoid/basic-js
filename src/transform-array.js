888aa9a64c8a7d471c1270e79fc0e6a821ef2073
FullCycleRoid <andrewthechf@gmail.com>
2020-10-06 16:00:28 +0300

    feat: 123

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  if (!(Array.isArray(arr))) {
+    throw new Error('THROW')
+  }
+
+  let newArr = arr
+
+  for (let i=0; i < newArr.length; i++) {
+    if (newArr[0] === '--discard-prev' || newArr[0] === '--double-prev') {
+      newArr.splice(0, 1)
+    }
+
+    if (newArr[newArr.length - 1] === '--discard-next' || newArr[newArr.length - 1] === '--double-next') {
+      newArr.splice(newArr.length-1, 1)
+    }
+
+    if (newArr[i] === '--discard-next') {
+      newArr.splice(i, 2)
+    } else if (newArr[i] === '--discard-prev') {
+      newArr.splice(i - 1, 2)
+    } else if (newArr[i] === '--double-next') {
+      newArr.splice(i, 0, newArr[i + 1])
+      newArr.splice(i, 1)
+    } else if (newArr[i] === '--double-prev') {
+      let temp = newArr[i - 1]
+      newArr.splice(i, 1)
+      newArr.splice(i, 0, temp)
+    }
+
+  }
+  return newArr
 };
+
+
+  //
+  //   }
+  // }
--- src/what-season.js
+++ src/what-season.js
@@ -1,26 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason( date ) {
+  console.log( date )
   if (date === undefined ) {
-    console.log(date)
     return 'Unable to determine the time of year!'
   }
-  if (!(date instanceof Date)) {
-    new Error('THROWN')
+  if (!(date instanceof Date) ) {
+    throw new Error('THROWN')
   }
 
-  let month = date.getMonth() + 1
-  console.log('date', date)
-  console.log('month', month)
-  console.log('==========================')
-  if (month <= 2) {
+  if (!(Object.getOwnPropertyNames(Date).includes('now'))) {
+      throw new Error('THROWN')
+  }
+
+  let month = date.getMonth()
+
+  if (month < 2 || month > 10) {
     return 'winter'
-  } else if  (month > 2 && month <= 5){
+  } else if  (month > 1 && month <= 4){
     return 'spring'
-  } else if  (month > 5 && month <= 9){
+  } else if  (month > 4 && month < 8){
     return 'summer'
   } else {
     return 'autumn'
   }
-
+  function isFunction(functionToCheck) {
+   return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+  }
 };
