435ed62900a7beffd46b9f56dee1d24943471311
FullCircleRoid <andrewthechf@gmail.com>
2020-10-11 20:09:26 +0300

    feat: add ciper and transform

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,10 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth( arr ) {
-          throw new CustomError('Not implemented') ;
-
-    // let res1 = arr.reduce((sum, elem) => sum + (Array.isArray(arr[elem]) ? this.calculateDepth(arr[elem]) : 1), 0)
-    // return res1
+      calculateDepth( arr, rec=1 ) {
+        let res = rec;
+        for(let i = 0; i < arr.length; ++i) {
+            if (Array.isArray(arr[i])) {
+            let subDepth = this.calculateDepth(arr[i], rec + 1);
+            if (subDepth > res) {
+                res = subDepth;
+            }
+          }
+        }
+        return res;
   }
+
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,64 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform( arr ) {
-      throw new CustomError('Not implemented') ;
-  //   if (!(Array.isArray(arr))) {
-  //       throw new Error('THROW')
-  //   }
-  //   let newArr = arr
-  //   let index = null
-  //
-  //   console.log(newArr)
-  //
-  //   let length = 0
-  //
-  //   for (let i = 0; i < newArr.length; i++) {
-  //       length = newArr.length
-  //
-  //       console.log('Length', length)
-  //
-  //       if (newArr[0] === '--discard-prev' || newArr[0] === '--double-prev') {
-  //           console.log('before deletion', newArr)
-  //           newArr.splice(0, 1)
-  //           console.log('after deletion', newArr)
-  //           i = 0
-  //           continue
-  //       }
-  //
-  //       if (newArr[newArr.length - 1] === '--discard-next' || newArr[newArr.length - 1] === '--double-next') {
-  //           console.log('before deletion', newArr)
-  //           newArr.splice(newArr.length - 1, 1)
-  //           i = 0
-  //           console.log('after deletion', newArr)
-  //           continue
-  //       }
-  //
-  //       if (newArr[i] === '--discard-next') {
-  //         console.log('before deletion', newArr)
-  //         newArr.splice(i, 2)
-  //         i = 0
-  //         console.log('after deletion', newArr)
-  //       } else if (newArr[i] === '--discard-prev') {
-  //         console.log('before deletion', newArr)
-  //         newArr.splice(i - 1, 2)
-  //         i = 0
-  //         console.log('after deletion', newArr)
-  //       } else if (newArr[i] === '--double-next') {
-  //         console.log('before double', newArr)
-  //         console.log('double value', newArr[i + 1])
-  //         newArr.splice(i+1, 0, newArr[i + 1])
-  //         newArr.splice(i, 1)
-  //         i = 0
-  //         console.log('after double', newArr)
-  //       } else if (newArr[i] === '--double-prev') {
-  //         console.log('before double', newArr)
-  //         console.log('double value', newArr[i-1])
-  //         newArr.splice(i, 1)
-  //         newArr.splice(i-1, 0, newArr[i-1])
-  //         i = 0
-  //         console.log('after double', newArr)
-  //       }
-  //   }
-  // console.log('after loop arrAy', newArr)
-  // return newArr
+      let discardNext = '--discard-next';
+    let discardPrev = '--discard-prev';
+    let doubleNext = '--double-next';
+    let doublePrev = '--double-prev';
+    let resultArray = [];
+    if(!Array.isArray(arr)){
+        throw new Error();
+    };
+    if(Array.isArray(arr) && arr.length === 0){
+        return [];
+    };
+    for (let i = 0; i < arr.length; i++){
+        switch(arr[i]) {
+            case discardNext: {
+                if(i + 1 < arr.length){
+                    i++;
+                };
+                break;
+            }
+
+            case discardPrev: {
+                if(i - 1 >= 0 && arr[i - 2] !== '--discard-next'){
+                    resultArray.pop();
+                };
+                break;
+            }
+
+            case doubleNext: {
+                if(i + 1 < arr.length){
+                    resultArray.push(arr[i + 1]);
+                };
+                break;
+            }
+
+            case doublePrev: {
+                if(i - 1 >= 0 && arr[i - 2] !== '--discard-next'){
+                    resultArray.push(arr[i - 1]);
+                };
+                break;
+            }
+
+            default: {
+                resultArray.push(arr[i]);
+            }
+        }
+    };
+    return resultArray;
 };
